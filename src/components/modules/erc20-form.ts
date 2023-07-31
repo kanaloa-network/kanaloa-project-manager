746d790835874c6bdf8224bc6cb0fc8e156bffe8
neirenoir <29525767+neirenoir@users.noreply.github.com>
2023-08-01 01:39:37 +0200

    Crazy? I was crazy once. They locked me in a room. A rubber room. A rubber room with rats. And rats make me crazy.

---
+++ src/components/modules/erc20-form.ts
@@ -0,0 +1,71 @@
+import { customElement } from "lit/decorators.js";
+import { ModuleForm } from "./commons";
+import { html } from "lit";
+import { Required, maxLengthPreprocessor, maxNumberPreprocessor } from "../forms";
+import { MinMaxLength, MinNumber, MaxNumber } from "@lion/form-core";
+import { MaxUint256 } from "ethers";
+
+@customElement('erc20-form')
+export class ERC20Form extends ModuleForm {
+
+    render() {
+        return html`
+            <div class="form-row">
+                <kana-input
+                    label-sr-only="Symbol"
+                    placeholder="Symbol"
+                    name="symbol"
+                    .validators="${[
+                        new MinMaxLength({ min: 2, max: 8}),
+                        new Required()
+                    ]}"
+                    .preprocessor=${maxLengthPreprocessor(8)}
+                    class="small-input"
+                ></kana-input>
+                <kana-select
+                    label-sr-only="Supply type"
+                    name="supply-type"
+                    placeholder="Supply type"
+                    .validators=${[ new Required() ]}
+                >
+                    <select name="supply-type" slot="input">
+                        <option hidden selected>
+                            Select supply type
+                        </option>
+                        <option value="fixed">
+                            Fixed supply
+                        </option>
+                        <option hidden value="variable">
+                            Variable supply
+                        </option>
+                    </select>
+                </kana-select>
+            </div>
+            <div class="form-row">
+                <kana-input-amount
+                    label-sr-only="Maximum supply"
+                    placeholder="Maximum supply"
+                    name="max-supply"
+                    .validators="${[
+                        new MinNumber(0),
+                        new MaxNumber(MaxUint256),
+                        new Required()
+                    ]}"
+                    .preprocessor=${maxNumberPreprocessor(MaxUint256)}
+                ></kana-input-amount>
+                <kana-input-stepper
+                    label="Decimals"
+                    label-sr-only="Token decimals"
+                    value="18"
+                    name="decimals"
+                    .validators="${[
+                        new Required()
+                    ]}"
+                    min="0"
+                    max="32"
+                ></kana-input-stepper>
+            </div>
+        `;
+    }
+    
+}
--- src/pages/new-contract-page.ts
+++ src/pages/new-contract-page.ts
@@ -1,139 +1,21 @@
 import { LitElement, html, css } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
-import "../components/forms"
-import "../components/windowlet"
+import { customElement, eventOptions, property } from 'lit/decorators.js';
+import "../components/forms";
+import "../components/windowlet";
 import "@lion/fieldset";
-import { MinMaxLength, Required } from "@lion/form-core"
+import { MinMaxLength } from "@lion/form-core";
 import { loadDefaultFeedbackMessages } from "@lion/validate-messages";
-import { KanaForm, KanaSelect, maxLengthPreprocessor } from '../components/forms';
+import { 
+    KanaForm, KanaSelect, Required, formCssCommon, maxLengthPreprocessor 
+} from '../components/forms';
+import { ModuleParams } from '../components/modules/commons';
 import { GlobalKanaloaEthers } from '../api/kanaloa-ethers';
 import { repeat } from 'lit/directives/repeat.js';
 import { KanaloaWindowlet } from '../components/windowlet';
-
-const cssCommon = [
-    css`
-        h1 {
-            font-size: 3rem;
-            display: block;
-            width: 100%;
-        }
-
-        h2 {
-            font-size: 2rem;
-            margin: 0.5rem 0 0.5rem;
-        }
-
-        h3 {
-            font-size: 1.5rem;
-            margin: 0;
-        }
-
-        hr {
-            border: none;
-            height: 2px;
-            background-color: var(--background-light-color);
-            margin: 0.5rem 0 1rem;
-        }
-
-        kana-input, input, kana-select {
-            flex: 1;
-            font-size: 1rem;
-            position: relative;
-        }
-
-        input, select {
-            font-family: sans;
-        }
-
-        input {
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            background-color: var(--primary-color);
-            color: var(--foreground-color);
-            box-sizing: border-box;
-        }
-        
-        input:focus {
-            outline: none;
-            box-shadow: 0 0 0 2px var(--highlighted-light-color);
-        }
-
-        .small-input, .small-input * {
-            flex: 0;
-        }
-
-        .small-input input {
-            width: 8rem;
-        }
-
-        .form-row {
-            display: flex;
-            gap: 1rem;
-            margin: 10px 0;
-            flex-flow: row wrap;
-        }
-
-        select {
-            padding: 10px;
-            padding-right: 2rem;
-            border: none;
-            border-radius: 5px;
-            background-color: var(--primary-color);
-            color: var(--foreground-color);
-            font-size: 1rem;
-            appearance: none;
-            cursor: pointer;
-            flex: 1;
-        }
-        
-        select:focus {
-            outline: none;
-            box-shadow: 0 0 0 2px var(--highlighted-light-color);
-        }
-        
-        kana-button-submit {
-            min-width: fit-content;
-            flex: 1;
-            font-size: 1.2rem;
-            min-height: 3rem;
-        }
-    `,
-    // There has to be a better place to put this, but I will figure 
-    // that out later
-    css`
-        .form-row lion-validation-feedback {
-            position: absolute;
-            background-color: var(--highlighted-light-color);
-            color: var(--background-color);
-            padding: 10px;
-            border-radius: 10px;
-            display: inline-block;
-            max-width: 12rem;
-            font-size: 0.8rem;
-            line-height: 1.2;
-            bottom: 2rem;
-            margin-left: -3rem;
-            width: max-content;
-            z-index: 1
-        }
-
-        .form-row lion-validation-feedback:not([type="error"]) {
-            display: none;
-        }
-        
-        .form-row lion-validation-feedback::before {
-            content: '';
-            position: absolute;
-            bottom: -18px;
-            left: 10%;
-            margin-left: -10px;
-            border: 10px solid transparent;
-            border-top: 15px solid var(--highlighted-light-color);
-        }
-        
-    `
-];
+import { ERC20Form } from '../components/modules/erc20-form';
+import { headerStyles } from '../components/common-styles';
+import { eventHandler, handlerSetup } from '../utils/event-handler';
+import { when } from 'lit/directives/when.js';
 
 @customElement('new-contract-page')
 export class NewContractPage extends LitElement {
@@ -142,26 +24,19 @@ export class NewContractPage extends LitElement {
     declare name: String;
     @property({ type: String })
     declare address: string;
-    @property({ type: Array })
-    declare basicModules: { name: string, value: string}[];
+    @property({ type: Boolean })
+    declare expandedMode: boolean;
 
     constructor() {
-        super();
-        
-        this.basicModules = [
-            { name: "ERC20", value: "erc20" },
-            { name: "ERC721", value: "erc721" }
-        ];
-
+        super();    
+        this.expandedMode = false;
+        handlerSetup(this);
         loadDefaultFeedbackMessages();
-        Required.getMessage = async () => {
-            return "Please, enter a value";
-        }
     }
 
     static get styles() {
         return [
-            ...cssCommon,
+            headerStyles,
             css`
                 :host {
                     display: flex;
@@ -172,7 +47,7 @@ export class NewContractPage extends LitElement {
                     flex: 1;
                 }
 
-                .form-expanded {
+                .expanded {
                     flex: 1 1 max-content;
                 }
 
@@ -185,6 +60,12 @@ export class NewContractPage extends LitElement {
                     flex: 0 1 max-content;
                     margin: 0 auto;
                     transition: flex 0.1s;
+                    gap: 1rem;
+                }
+                
+                .contract-overview-group {
+                    display: flex;
+                    flex-flow: column wrap;
                     gap: 1rem;
                 }
             `
@@ -214,94 +95,163 @@ export class NewContractPage extends LitElement {
         // });
     }
 
-    selectContractType(ev: Event) {
+    @eventHandler("base-selected", { once: true })
+    expandForm(ev: Event) {
+        this.expandedMode = true;
+    }
+
+    @eventHandler("base-selected", { capture: true })
+    baseTypeSelected(ev: Event) {
         const selected: string = (ev.target as KanaSelect).value;
-        const form = 
-            (ev.target as HTMLElement).closest("form") as HTMLFormElement;
-        if (form.classList.contains("form-expanded")) {
-            return;
-        }
-        form.classList.add("form-expanded");
-        // We should asyncronously load the module-specific form data here
-        // Or not, because lazy loading components sucks
     }
 
     render() {
         return html`
-            <h1 id="page-title">New contract for ${this.name}</h1>
-            <kana-form @submit="${this.submitHandler}">
-                <form @submit=${(ev: Event) => ev.preventDefault()}>
-                    <lion-fieldset name="base-module-config">
-                        <kana-windowlet>
-                            <h2>New contract</h2>
-                            <hr />
-                            <label>Project info</label>
-                            <div class="form-row">
-                                <kana-input
-                                    label-sr-only="Contract name"
-                                    placeholder="Contract name"
-                                    name="name"
-                                    .validators="${[
-                                        new MinMaxLength({ min: 4, max: 16}),
-                                        new Required()
-                                    ]}"
-                                    .preprocessor=${maxLengthPreprocessor(16)}
-                                ></kana-input>
-                                <kana-select
-                                    label-sr-only="Contract type"
-                                    name="type"
-                                    placeholder="Contract type"
-                                    class="small-input"
-                                    .validators=${[ new Required() ]}
-                                    @change=${this.selectContractType}
-                                >
-                                    <select name="type-select" slot="input">
-                                        <option 
-                                            hidden
-                                            selected
-                                            value
-                                        >
-                                            Select type
-                                        </option>
-                                        ${repeat(
-                                            this.basicModules, 
-                                            (k: any) => k.value, 
-                                            (item: any) => {
-                                                return html`
-                                                    <option
-                                                        name="${item.name}"
-                                                        value="${item.value}"
-                                                    >
-                                                        ${item.name}
-                                                    </option>
-                                                `;
-                                            }
-                                        )}
-                                    </select>
-                                </kana-select>
-                            </div>
-                            <lion-fieldset
-                                id="type-specific-config"
-                                name="type-specific-config" 
-                                class="form-row" 
-                                hidden
-                            >
+            ${
+                when(
+                    !this.expandedMode,
+                    () => html`
+                        <h1 id="page-title">
+                            New contract for ${this.name}
+                        </h1>
+                    `
+                )
 
-                            </lion-fieldset>
-                            <div class="form-row">
-                                <kana-button-submit>
-                                    Deploy new project
-                                </kana-button-submit>
-                            </div>
-                        </kana-windowlet>
-                    </lion-fieldset> 
+            }
+            <kana-form @submit="${this.submitHandler}">
+                <form 
+                    @submit=${(ev: Event) => ev.preventDefault()}
+                    class="${this.expandedMode ? "expanded" : ""}"
+                >
+                    <div class="contract-overview-group">
+                        <lion-fieldset name="base-module-config">
+                            <new-contract-base-windowlet>
+                            </new-contract-base-windowlet>
+                        </lion-fieldset>
+                        ${
+                          (this.expandedMode) ?
+                            html`
+                                <modules-installed-windowlet
+                                    class="${this.expandedMode ? "expanded" : ""}"
+                                >
+                                </modules-installed-windowlet>
+                            ` : ""
+                        }
+                    </div> 
                 </form>
             </kana-form>
         `;
     }
 }
 
+@customElement('new-contract-base-windowlet')
+export class NewContractBaseWindowlet extends KanaloaWindowlet {
+
+    basicModules: ModuleParams[];
+
+    constructor() {
+        super();
+
+        this.basicModules = [
+            { name: "ERC20", value: "erc20", form: ERC20Form },
+            { name: "ERC721", value: "erc721", form: LitElement }
+        ];
+    }
+
+    static get styles() {
+        return [
+            ...super.styles,
+            ...formCssCommon
+        ]
+    }
+
+    selectContractType(ev: Event) {
+        this.dispatchEvent(
+            new CustomEvent(
+                "base-selected", 
+                { bubbles: true, composed: true }
+            )
+        );
+
+        // const moduleData = 
+        //     this.basicModules.find((module) => module.value == selected);
+        // if (moduleData == null) {
+        //     return;
+        // }
+
+        // console.log(document.getElementById("type-specific-config"))
+        // this.querySelector("#type-specific-config")?.append(new moduleData.form())
+        // We should asyncronously load the module-specific form data here
+        // Or not, because lazy loading components sucks
+    }
+
+    render() {
+        return html`
+            <h2 id="contract-title">New contract</h2>
+            <hr />
+            <label>Project info</label>
+            <div class="form-row">
+                <kana-input
+                    label-sr-only="Contract name"
+                    placeholder="Contract name"
+                    name="name"
+                    .validators="${[
+                        new MinMaxLength({ min: 4, max: 16}),
+                        new Required()
+                    ]}"
+                    .preprocessor=${maxLengthPreprocessor(16)}
+                ></kana-input>
+                <kana-select
+                    label-sr-only="Contract type"
+                    name="type"
+                    placeholder="Contract type"
+                    class="small-input"
+                    .validators=${[ new Required() ]}
+                    @change=${this.selectContractType}
+                >
+                    <select name="type-select" slot="input">
+                        <option hidden selected>
+                            Select type
+                        </option>
+                        ${repeat(
+                            this.basicModules, 
+                            (k: any) => k.value, 
+                            (item: any) => {
+                                return html`
+                                    <option
+                                        name="${item.name}"
+                                        value="${item.value}"
+                                    >
+                                        ${item.name}
+                                    </option>
+                                `;
+                            }
+                        )}
+                    </select>
+                </kana-select>
+            </div>
+            <lion-fieldset
+                id="type-specific-config"
+                name="type-specific-config" 
+                class="form-row" 
+            >
+            </lion-fieldset>
+            <div class="form-row">
+                <kana-button-submit>
+                    Deploy new project
+                </kana-button-submit>
+            </div>
+        `;
+    }
+}
+
 @customElement('modules-installed-windowlet')
 export class ModulesInstalledWindowlet extends KanaloaWindowlet {
+    constructor() {
+        super();
+    }
 
+    render() {
+        return html`hello world`;
+    }
 }
